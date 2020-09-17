6c7f829ae7848755e01f85239fadf73dcc0a57e9
thanh.lv97 <lethanh9398@gmail.com>
2020-09-17 13:56:04 +0700

    adđ

--- src/main/java/com/lethanh98/archdemo/utils/NIO.java
+++ src/main/java/com/lethanh98/archdemo/utils/NIO.java
@@ -4,12 +4,21 @@ import java.util.Arrays;
 import java.util.List;
 
 public class NIO {
-    public static void runMultilTaskSync(Runnable... runnables) {
+    // chạy bất đồng bộ nhưng chờ chạy xong
+    public static void runMultipleTaskSync(Runnable... runnables) {
         List<Runnable> runnableList = Arrays.asList(runnables);
-        runnableList.parallelStream().forEach(Runnable::run);
+        runMultipleTaskSync(runnableList);
+    }
+    // chạy bất đồng bộ không chờ chạy xong
+    public static void runMultipleTaskASync(Runnable... runnables) {
+        List<Runnable> runnableList = Arrays.asList(runnables);
+        runMultipleTaskASync(runnableList);
     }
 
-    public static void runMultilTaskSync(List<Runnable> runnables) {
+    public static void runMultipleTaskSync(List<Runnable> runnables) {
         runnables.parallelStream().forEach(Runnable::run);
+    }
+    public static void runMultipleTaskASync(List<Runnable> runnables) {
+        runnables.parallelStream().forEach(runnable1 -> new Thread(runnable1).start());
     }
 }
