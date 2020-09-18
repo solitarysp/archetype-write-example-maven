bd811916dfd0fd8c3a728c1abba351ea429ad55c
thanh.lv97 <lethanh9398@gmail.com>
2020-09-18 09:54:25 +0700

    1

--- src/main/java/com/lethanh98/archdemo/MainApp.java
+++ src/main/java/com/lethanh98/archdemo/MainApp.java
@@ -2,6 +2,7 @@ package com.lethanh98.archdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
